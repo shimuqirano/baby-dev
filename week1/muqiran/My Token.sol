33dbab9fd9a341903039578bc9ff0f7b6d160fa6
muqiran <105968165+shimuqirano@users.noreply.github.com>
2025-11-09 16:32:47 +0800

    Create My Token.sol

---
+++ week1/muqiran/My Token.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("muqiran", "qiran") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
